b03d4fd286260069c0f6b7f8742588736db9a79d
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2024-04-08 13:24:22 +0600

    Filtering Projects

--- project-manage-app/resources/js/Pages/Project/Index.jsx
+++ project-manage-app/resources/js/Pages/Project/Index.jsx
@@ -1,9 +1,27 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+    queryParams = queryParams || {}; // If null, then it's null else assigned an empty object
+    const searchFieldChanged = (paraName, value) => {
+        if (value) {
+            queryParams[paraName] = value;
+        } else {
+            delete queryParams[paraName];
+        }
+        // Redirect with param values
+        router.get(route("project.index", queryParams));
+    };
+
+    const onKeyPress = (name, e) => {
+        if (e.key !== "Enter") return;
+        searchFieldChanged(name, e.target.value);
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -36,6 +54,61 @@ export default function Index({ auth, projects }) {
                                         <th className="px-3 py-3 text-right">
                                             Action
                                         </th>
+                                    </tr>
+                                </thead>
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th
+                                            className="px-3 py-3"
+                                            colSpan={2}
+                                        ></th>
+                                        <th className="px-3 py-3">
+                                            <TextInput
+                                                className="w-full"
+                                                defaultValue={queryParams.name}
+                                                placeholder="Project Name"
+                                                onBlur={(e) =>
+                                                    searchFieldChanged(
+                                                        "name",
+                                                        e.target.value
+                                                    )
+                                                }
+                                                onKeyPress={(e) =>
+                                                    onKeyPress("name", e)
+                                                }
+                                            />
+                                        </th>
+                                        <th className="px-3 py-3">
+                                            <SelectInput
+                                                className="w-full"
+                                                defaultValue={
+                                                    queryParams.status
+                                                }
+                                                onChange={(e) =>
+                                                    searchFieldChanged(
+                                                        "status",
+                                                        e.target.value
+                                                    )
+                                                }
+                                            >
+                                                <option value="">
+                                                    Select Status
+                                                </option>
+                                                <option value="pending">
+                                                    Pending
+                                                </option>
+                                                <option value="in_progress">
+                                                    In Progress
+                                                </option>
+                                                <option value="completed">
+                                                    Completed
+                                                </option>
+                                            </SelectInput>
+                                        </th>
+                                        <th
+                                            className="px-3 py-3"
+                                            colSpan={4}
+                                        ></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -74,10 +147,10 @@ export default function Index({ auth, projects }) {
                                                     }
                                                 </span>
                                             </td>
-                                            <td className="px-3 py-2">
+                                            <td className="px-3 py-2 text-nowrap">
                                                 {project.created_at}
                                             </td>
-                                            <td className="px-3 py-2">
+                                            <td className="px-3 py-2 text-nowrap">
                                                 {project.due_date}
                                             </td>
                                             <td className="px-3 py-2">
